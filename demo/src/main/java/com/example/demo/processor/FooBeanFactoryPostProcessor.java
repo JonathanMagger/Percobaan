ff7c7ea0164f18d56c2c07e8a27b3aeadbebd2b7
unknown <jonathan.world.changer@gmail.com>
2023-03-26 13:44:49 +0700

    commit 26/3

---
+++ demo/src/main/java/com/example/demo/processor/FooBeanFactoryPostProcessor.java
@@ -0,0 +1,2 @@
+package com.example.demo.processor;public class FooBeanFactoryPostProcessor {
+}
---
+++ demo/src/main/java/com/example/demo/processor/PrefixIdGeneratorBeanPostProcessor.java
@@ -0,0 +1,34 @@
+package com.example.demo.processor;
+
+import com.example.demo.aware.IdAware;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class IdGeneratorBeanPostProcessor implements BeanPostProcessor, Ordered {
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+       log.info("id Generator Processor for bean {}",beanName);
+        if (bean instanceof IdAware){
+            log.info("Set Id Generator for Bean {}",beanName);
+            IdAware idAware = (IdAware) bean;
+            idAware.setId(UUID.randomUUID().toString());
+        }
+
+        return bean;
+    }
+
+
+}
